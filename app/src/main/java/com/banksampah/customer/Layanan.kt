a0c5948703cafae651d1daf5ed75fa7ba072280e
SeptianWahyuni <septianwahyuni76@gmail.com>
2020-06-13 20:27:49 +0700

    lanjutan

---
+++ app/src/main/java/com/banksampah/customer/Layanan.kt
@@ -0,0 +1,11 @@
+package com.banksampah.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Layanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layanan)
+    }
+}
